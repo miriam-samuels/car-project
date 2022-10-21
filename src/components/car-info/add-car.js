b2d99489e5eb7219631dec4e305fb6816fa09340
miriam-samuels <medomiriam@gmail.com>
2022-10-21 09:07:42 +0100

    design chanhes

--- src/components/car-info/add-car.js
+++ src/components/car-info/add-car.js
@@ -1,43 +1,92 @@
-function AddCar() {
+import React, { useState } from "react";
+function AddCar(props) {
+   const [carInfo, setCarInfo] = useState({
+      id:"",
+      first_name: "",
+      last_name: "",
+      email: "",
+      gender: "",
+      car_make: "",
+      car_model: "",
+      car_model_year: "",
+      car_vin: "",
+      avatar: ""
+   })
+   const {data, setData} = props
+
+   const handleChange = (e) => {
+      const val = e.target.value;
+      const field = e.target.id
+
+      setCarInfo({
+         ...carInfo,
+         [field]: val
+      })
+
+      console.log(carInfo);
+   }
+
+   const handleSubmit = (e) => {
+      e.preventDefault()
+
+      const newData = { ...carInfo, id : `${carInfo.car_vin}-${carInfo.car_model}-${carInfo.car_model_year}`}
+      setData([
+         newData,
+         ...data,
+      ])
+      setCarInfo({
+         id:"",
+         first_name: "",
+         last_name: "",
+         email: "",
+         gender: "",
+         car_make: "",
+         car_model: "",
+         car_model_year: "",
+         car_vin: "",
+         avatar: ""
+      })
+   }
    return (
-      <form>
+      <form onSubmit={handleSubmit}>
          <div>
             <label htmlFor='first_name'>First Name</label>
-            <input type='text' id='first_name' placeholder='Enter First Name' />
+            <input type='text' id='first_name' placeholder='Enter First Name' onChange={handleChange} value={carInfo.first_name} />
          </div>
          <div>
             <label htmlFor='last_name'>Last Name</label>
-            <input type='text' id='last_name' placeholder='Enter Last Name' />
+            <input type='text' id='last_name' placeholder='Enter Last Name' onChange={handleChange} value={carInfo.last_name} />
          </div>
          <div>
             <label htmlFor='email'>Email</label>
-            <input type='email' id='email' placeholder='Enter Email' />
+            <input type='email' id='email' placeholder='Enter Email' onChange={handleChange} value={carInfo.email} />
          </div>
          <div>
             <label htmlFor='gender'>Gender</label>
-            <input type='text' id='gender' placeholder='Enter Gender' />
+            <input type='text' id='gender' placeholder='Enter Gender' onChange={handleChange} value={carInfo.gender} />
          </div>
          <div>
             <label htmlFor='car_make'>Car Make</label>
-            <input type='text' id='car_make' placeholder='Enter Car Make' />
+            <input type='text' id='car_make' placeholder='Enter Car Make' onChange={handleChange} value={carInfo.car_make} />
          </div>
          <div>
             <label htmlFor='car_model'>Car Model</label>
-            <input type='text' id='car_model' placeholder='Enter Car Model' />
+            <input type='text' id='car_model' placeholder='Enter Car Model' onChange={handleChange} value={carInfo.car_model} />
          </div>
          <div>
             <label htmlFor='car_model_year'>Car Model Year</label>
-            <input type='number' id='car_model_year' placeholder='Enter Car Model Year' />
+            <input type='number' id='car_model_year' placeholder='Enter Car Model Year' onChange={handleChange} value={carInfo.car_model_year} />
          </div>
          <div>
             <label htmlFor='car_vin'>Car VIN</label>
-            <input type='text' id='car_vin' placeholder='Enter Car Vin' />
+            <input type='text' id='car_vin' placeholder='Enter Car Vin' onChange={handleChange} value={carInfo.car_vin} />
          </div>
          <div>
             <label htmlFor='avatar'>Avatar</label>
-            <input type='url' id='avatar' placeholder='Enter Link to Avatar' />
+            <input type='url' id='avatar' placeholder='Enter Link to Avatar' onChange={handleChange} value={carInfo.avatar} />
          </div>
          <div> <button type="submit">Add Entry</button> </div>
       </form>
-   )}
+   )
+}
 export default AddCar;
--- src/components/car-info/index.js
+++ src/components/car-info/index.js
@@ -1,12 +1,56 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import AddCar from './add-car'
 import { carData } from './car-data'
 
 function CarInfo() {
+   const [isVisible, setIsVisible] = useState(false)
+   const [carDatas, setCarDatas] = useState(carData)
+   const [filtered, setFiltered] = useState(carDatas)
+   const [filterBy, setFilterBy] = useState("0")
+   const [searchText, setSearchText] = useState("")
+
+   useEffect(() => {
+      filterEntries()
+   }, [searchText])
+
+
+   const handleDelete = (id) => {
+      setCarDatas(carDatas.filter((item) => item.id !== id))
+   }
+
+   const filterEntries = () => {
+      console.log(searchText)
+      switch (filterBy) {
+         case "1":
+            setFiltered(carDatas.filter((item) => item.car_make.toLowerCase().includes(searchText.toLowerCase())))
+            break;
+         case "2":
+            setFiltered(carDatas.filter((item) => item.car_model.toLowerCase().includes(searchText.toLowerCase())))
+            break;
+         default:
+            setFiltered(carDatas.filter((item) => item.email.toLowerCase().includes(searchText.toLowerCase())))
+            break;
+      }
+   }
+
    return (
       <div>
          <div className='page-heading'>
             <h1>PEOPLE AND THEIR CARS</h1>
+         </div>
+         <div>
+            {isVisible && <AddCar data={carDatas} setData={(val) => setCarDatas(val)} />}
+         </div>
+         <div className='actions'>
+            <button className='show-button' onClick={() => setIsVisible(current => !current)}>{isVisible ? "Hide Form" : "Show Form "}</button>
+            <div className="filter">
+               <select onChange={(e) => setFilterBy(e.target.value)} value={filterBy}>
+                  <option value="0">Email</option>
+                  <option value="1">Car Make</option>
+                  <option value="2">Car Model</option>
+               </select>
+               <input placeholder='search...' value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+            </div>
          </div>
          <div>
             <table>
@@ -21,11 +65,12 @@ function CarInfo() {
                      <th>Car Model</th>
                      <th>Car Model Year</th>
                      <th>Car VIN</th>
+                     <th>Actions</th>
                   </tr>
                </thead>
                <tbody>
                   {
-                     carData.map((elem) => (
+                     filtered.map((elem) => (
                         <tr key={elem.id}>
                            <td><img src={elem.avatar} alt="img" /></td>
                            <td>{elem.first_name}</td>
@@ -36,6 +81,7 @@ function CarInfo() {
                            <td>{elem.car_model}</td>
                            <td>{elem.car_model_year}</td>
                            <td>{elem.car_vin}</td>
+                           <td><button onClick={() => handleDelete(elem.id)}>Delete</button></td>
                         </tr>
                      ))
                   }
